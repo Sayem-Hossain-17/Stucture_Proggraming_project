456367dc0a2f78b46f44a96d7214a6035abba295
Sayem-Hossain-17 <158332221+Sayem-Hossain-17@users.noreply.github.com>
2024-04-22 11:56:48 +0600

    Update graph.c

--- graph.c
+++ graph.c
@@ -1,165 +1,160 @@
-implements graphs with its traingle found oparation
+Sure, here's your code with proper variable names and naming conventions applied:
+
+```c
 #include <stdio.h>
 
 // Define a struct to represent a node in the graph
 struct Node {
-    int nodeid;
-    int adjcount;
-    int cost[10];
-    int costs[10];
+    int nodeId;
+    int adjCount;
+    int adjacentNodes[10];
+    int edgeCosts[10];
 };
 
 // Function prototypes
-int chacktringle(struct Node* p, int count);
-int addNode(struct Node* p, int nid, int count);
-void addAdjacent(struct Node* p, int nid1, int nid2, int cost, int count);
-int added(int* list, int lcount, int nid);
-void findpath(struct Node* p, int count, int start, int end, int* list, int* clist, int lcount);
+int checkTriangle(struct Node* nodes, int nodeCount);
+int addNode(struct Node* nodes, int nodeId, int nodeCount);
+void addAdjacent(struct Node* nodes, int nodeId1, int nodeId2, int cost, int nodeCount);
+int isAdded(int* list, int listCount, int nodeId);
+void findPath(struct Node* nodes, int nodeCount, int start, int end, int* pathList, int* costList, int pathCount);
 
 int main() {
     struct Node nodes[50]; // Array to hold nodes
-    int nodecount = 0; // Counter for number of nodes
-    int n1 = 0, n2 = 0, c = 0; // Variables for input
-    
+    int nodeCount = 0; // Counter for number of nodes
+    int node1 = 0, node2 = 0, cost = 0; // Variables for input
+
     // Loop to input nodes and edges
     while (1) {
         printf("Enter n1, n2, cost (or -9 to exit): ");
         scanf("%d %d %d", &n1, &n2, &c);
-        if (n1 == -9 || n2 == -9 || c == -9) {
+        if (node1 == -9 || node2 == -9 || cost == -9) {
             break;
         }
-        nodecount = addNode(&nodes[0], n1, nodecount);
-        nodecount = addNode(&nodes[0], n2, nodecount);
-        addAdjacent(&nodes[0], n1, n2, c, nodecount);
-        addAdjacent(&nodes[0], n2, n1, c, nodecount);
+        nodeCount = addNode(nodes, node1, nodeCount);
+        nodeCount = addNode(nodes, node2, nodeCount);
+        addAdjacent(nodes, node1, node2, cost, nodeCount);
+        addAdjacent(nodes, node2, node1, cost, nodeCount);
     }
 
     // Input start and end nodes
     int start, end;
     printf("Enter start and end node: ");
     scanf("%d %d", &start, &end);
-    
+
     // Array to store path and costs
-    int list[50], clist[50], lcount = 0;
-    list[0] = start; clist[0] = 0; lcount = 1;
+    int pathList[50], costList[50], pathCount = 0;
+    pathList[0] = start; costList[0] = 0; pathCount = 1;
 
     // Find paths from start to end
-    findpath(nodes, nodecount, start, end, list, clist, lcount);
-    
+    findPath(nodes, nodeCount, start, end, pathList, costList, pathCount);
+
     // Output the number of triangles in the graph
-    int triangleCount = chacktringle(nodes, nodecount);
+    int triangleCount = checkTriangle(nodes, nodeCount);
     printf("Number of triangles: %d\n", triangleCount);
-    
+
     return 0;
 }
 
 // Function to check for triangles in the graph
-int chacktringle(struct Node* p, int count) {
-    int tcount = 0;
-    
+int checkTriangle(struct Node* nodes, int nodeCount) {
+    int triangleCount = 0;
+
     // Loop through all combinations of three nodes
-    for (int i = 0; i < count; i++) {
-        for (int j = i + 1; j < count; j++) {
-            for (int k = j + 1; k < count; k++) {
-                int node1 = p[i].nodeid;
-                int node2 = p[j].nodeid;
-                int node3 = p[k].nodeid;
-                int e1 = 0, e2 = 0, e3 = 0;
+    for (int i = 0; i < nodeCount; i++) {
+        for (int j = i + 1; j < nodeCount; j++) {
+            for (int k = j + 1; k < nodeCount; k++) {
+                int node1 = nodes[i].nodeId;
+                int node2 = nodes[j].nodeId;
+                int node3 = nodes[k].nodeId;
+                int edge1 = 0, edge2 = 0, edge3 = 0;
 
                 // Check if each pair of nodes shares an edge
-                for (int x = 0; x < p[i].adjcount; x++) {
-                    if ((p[i].cost[x] == node2) || (p[i].cost[x] == node3)) {
-                        e1++;
+                for (int x = 0; x < nodes[i].adjCount; x++) {
+                    if ((nodes[i].adjacentNodes[x] == node2) || (nodes[i].adjacentNodes[x] == node3)) {
+                        edge1++;
                     }
                 }
-                for (int y = 0; y < p[j].adjcount; y++) {
-                    if ((p[j].cost[y] == node1) || (p[j].cost[y] == node3)) {
-                        e2++;
+                for (int y = 0; y < nodes[j].adjCount; y++) {
+                    if ((nodes[j].adjacentNodes[y] == node1) || (nodes[j].adjacentNodes[y] == node3)) {
+                        edge2++;
                     }
                 }
-                for (int z = 0; z < p[k].adjcount; z++) {
-                    if ((p[k].cost[z] == node1) || (p[k].cost[z] == node2)) {
-                        e3++;
+                for (int z = 0; z < nodes[k].adjCount; z++) {
+                    if ((nodes[k].adjacentNodes[z] == node1) || (nodes[k].adjacentNodes[z] == node2)) {
+                        edge3++;
                     }
                 }
                 // If all three nodes form a triangle, increment the triangle count
                 if (e1 == 2 && e2 == 2 && e3 == 2) {
                     printf("Triangle nodes: %d %d %d\n", node1, node2, node3);
-                    tcount++;
+                    triangleCount++;
                 }
             }
         }
     }
-    return tcount;
+    return triangleCount;
 }
 
 // Function to add a node to the graph
-int addNode(struct Node* p, int nid, int count) {
-    int i = 0, ncount = count;
-    for (i = 0; i < count; i++) {
-        if (p[i].nodeid == nid) { break; }
+int addNode(struct Node* nodes, int nodeId, int nodeCount) {
+    int i = 0, newNodeCount = nodeCount;
+    for (i = 0; i < nodeCount; i++) {
+        if (nodes[i].nodeId == nodeId) { break; }
     }
-    if (i == count) {
-        p[i].nodeid = nid;
-        p[i].adjcount = 0;
-        ncount++;
+    if (i == nodeCount) {
+        nodes[i].nodeId = nodeId;
+        nodes[i].adjCount = 0;
+        newNodeCount++;
     }
-    return ncount;
+    return newNodeCount;
 }
 
 // Function to add an adjacent node and its cost to a given node
-void addAdjacent(struct Node* p, int nid1, int nid2, int cost, int count) {
+void addAdjacent(struct Node* nodes, int nodeId1, int nodeId2, int cost, int nodeCount) {
     int i = 0, index;
-    for (i = 0; i < count; i++) {
-        if (p[i].nodeid == nid1) {
-            int c = p[i].adjcount;
-            p[i].cost[c] = nid2;
-            p[i].costs[c] = cost;
-            p[i].adjcount = c + 1;
+    for (i = 0; i < nodeCount; i++) {
+        if (nodes[i].nodeId == nodeId1) {
+            int c = nodes[i].adjCount;
+            nodes[i].adjacentNodes[c] = nodeId2;
+            nodes[i].edgeCosts[c] = cost;
+            nodes[i].adjCount = c + 1;
             break;
         }
     }
 }
 
 // Function to check if a node is already added to the list
-int added(int* list, int lcount, int nid) {
-    int i = 0;
-    for (i = 0; i < lcount; i++) {
-        if (list[i] == nid) { return 1; }
+int isAdded(int* list, int listCount, int nodeId) {
+    for (int i = 0; i < listCount; i++) {
+        if (list[i] == nodeId) { return 1; }
     }
     return 0;
 }
 
 // Function to find all possible paths from start to end node
-void findpath(struct Node* p, int count, int start, int end, int* list, int* clist, int lcount) {
+void findPath(struct Node* nodes, int nodeCount, int start, int end, int* pathList, int* costList, int pathCount) {
     int index = 0, i = 0, j = 0, k = 0;
 
     // Check if list contains the end node
-    if (list[lcount - 1] == end) {
-        int tcost = 0;
+    if (pathList[pathCount - 1] == end) {
+        int totalCost = 0;
         printf("\nPath: ");
-        for (i = 0; i < lcount; i++) {
-            printf("%d ", list[i]);
-            tcost += clist[i];
+        for (i = 0; i < pathCount; i++) {
+            printf("%d ", pathList[i]);
+            totalCost += costList[i];
         }
-        printf(" Cost = %d\n", tcost);
+        printf(" Cost = %d\n", totalCost);
         return;
     }
 
     // Find the index of the starting node
-    for (i = 0; i < count; i++) {
-        if (p[i].nodeid == start) { index = i; break; }
+    for (i = 0; i < nodeCount; i++) {
+        if (nodes[i].nodeId == start) { index = i; break; }
     }
 
     // Iterate through adjacent nodes and recursively find paths
-    for (i = 0; i < p[index].adjcount; i++) {
-        int a = added(list, lcount, p[index].cost[i]);
-        if (a == 0) {
-            list[lcount] = p[index].cost[i];
-            clist[lcount] = p[index].costs[i];
-            lcount++;
-            findpath(p, count, p[index].cost[i], end, list, clist, lcount);
-            lcount--;
-        }
-    }
-}
+    for (i = 0; i < nodes[index].adjCount; i++) {
+        int added = isAdded(pathList, pathCount, nodes[index].adjacentNodes[i]);
+        if (!added) {
+            pathList[pathCount] = nodes[index].adjacentNodes[i];
+            costList[pathCount
